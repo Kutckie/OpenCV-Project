e73adf13379c471c72a382071a156d4e11fc4e13
Kutckie <milomil@inbox.ru>
2021-09-21 22:53:33 +0300

    init

---
+++ main.py
@@ -0,0 +1,9 @@
+#default imports
+#GUI dependencies
+import PyQt5
+from PyQt5.QtGui import QFont
+from PyQt5.QtCore import Qrect, Qt
+from PyQt5.QtWidgets import (QCheckBox, QApplication, QWidget, QMainWindow, QLabel, QPushButton, QTextBrowser)
+
+from threading import Thread
+from multiprocessing import Process, Value, Array, freeze_support
